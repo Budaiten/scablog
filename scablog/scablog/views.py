677814cd7362ae8c41550a08975200d81b496611
Budaiten <agustinsilvaa@gmail.com>
2023-06-30 10:05:44 -0300

    primer commit

--- scablog/scablog/views.py
+++ scablog/scablog/views.py
@@ -1,4 +1,10 @@
+<<<<<<< HEAD
 from django.shortcuts import render
 
 def index(request):
+=======
+from django.shortcuts import render
+
+def index(request):
+>>>>>>> 716ef49 (primer commit)
     return render(request, 'index.html')
