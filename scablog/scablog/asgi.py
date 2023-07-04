65d88dbf585c378a727ff158bf3180e1b93eec01
Juan Agustín Silva <agustinsilvaa@gmail.com>
2023-07-04 11:44:01 -0300

    commit urls

--- scablog/scablog/asgi.py
+++ scablog/scablog/asgi.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 ASGI config for scablog project.
 
@@ -15,21 +14,4 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scablog.settings')
 
 application = get_asgi_application()
-=======
-"""
-ASGI config for scablog project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
-import os
-
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scablog.settings')
-
-application = get_asgi_application()
->>>>>>> 716ef49 (primer commit)
--- scablog/scablog/views.py
+++ scablog/scablog/views.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 from django.shortcuts import render
 
 def index(request):
-=======
-from django.shortcuts import render
-
-def index(request):
->>>>>>> 716ef49 (primer commit)
     return render(request, 'index.html')
